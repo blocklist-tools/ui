14b5fba1d9c2c3c8a6c1add7daaf774a14132a1a
Daniel <daniel@developerdan.com>
2020-08-25 16:32:08 +0000

    Added logic for initial version and current version

--- src/components/entry-search-response/DisplayEntrySearchResponse.tsx
+++ src/components/entry-search-response/DisplayEntrySearchResponse.tsx
@@ -3,71 +3,83 @@ import {EntrySearchResponse, EntrySummary} from "../../services/ApiClient";
 import "./DisplayEntrySearchResponse.css";
 
 interface IDisplayEntrySearchResponse {
-    entrySearchResponse: EntrySearchResponse|null
+  entrySearchResponse: EntrySearchResponse | null
 }
 
-function dateSpan(date: Date) {
-    return (<span title={date.toISOString()}>{date.toDateString()}</span>)
+function dateSpan(date: Date|null, altText: string | null) {
+  let text = '';
+  let title = null;
+  if (date) {
+    text = date.toDateString();
+    title = date.toISOString();
+  }
+  if (altText) {
+    text = altText;
+  }
+  title = title || text;
+  return (<span title={title}>{text}</span>)
 }
 
 function includedIcon(entry: EntrySummary) {
-    const isIncluded = entry.removedVersionId !== null;
-    const className = [
-        'included-icon',
-        isIncluded ? 'is-removed' : 'is-included'
-    ].join(' ');
-    const icon = isIncluded ? '🚫' : '✔';
+  const isIncluded = entry.removedVersionId !== null;
+  const className = [
+    'included-icon',
+    isIncluded ? 'is-removed' : 'is-included'
+  ].join(' ');
+  const icon = isIncluded ? '🚫' : '✔';
 
-    return (<span className={className}>{icon}</span>);
+  return (<span className={className}>{icon}</span>);
 }
 
 function entryStatus(entry: EntrySummary) {
-    const isIncluded = entry.removedVersionId !== null;
-
-    let blockedStatusHtml = (<span>Current</span>);
-    if (isIncluded) {
-        blockedStatusHtml = dateSpan(entry.removedOn);
-    }
-    return (
-        <div>
-            {includedIcon(entry)}
-            {dateSpan(entry.addedOn)} - {blockedStatusHtml}
-        </div>
-    );
+  let stillBlocked = null;
+  if (!entry.removedOn) {
+    stillBlocked = 'Current Version';
+  }
+  let initialVersion = null;
+  if (entry.addedOn.toISOString() === entry.listAddedOn.toISOString()) {
+    initialVersion = 'Initial Version';
+  }
+  return (
+    <div>
+      {includedIcon(entry)}
+      {dateSpan(entry.addedOn, initialVersion)} - {dateSpan(entry.removedOn, stillBlocked)}
+    </div>
+  );
 }
 
-export const DisplayEntrySearchResponse: FunctionComponent<IDisplayEntrySearchResponse> = (props: IDisplayEntrySearchResponse) =>  {
-    if (!props.entrySearchResponse) {
-        return null;
-    }
-    let content = null
-    if (props.entrySearchResponse.summaries.length === 0) {
-        content = (
-            <div>No known list contains {props.entrySearchResponse.query}</div>
-        );
-    } else {
-        const listItems = props.entrySearchResponse.summaries.map((entry) => {
-            return (
-                <li key={entry.addedVersionId}>
-                    <div className={'blocklist-name'}>{entry.blocklistName}</div>
-                    {entryStatus(entry)}
-                </li>
-            );
-        });
-        content = (
-            <div>
-                <p>Blocklists that contain a matching entry, including the date it was first added, and optionally removed.</p>
-                <ul>{listItems}</ul>
-            </div>
-        )
-    }
-
-    return (
-        <section className={'component-display-entry-search-response'}>
-            <h2>
-                Lists containing {props.entrySearchResponse.query}
-            </h2>
-            {content}
-        </section>
+export const DisplayEntrySearchResponse: FunctionComponent<IDisplayEntrySearchResponse> = (props: IDisplayEntrySearchResponse) => {
+  if (!props.entrySearchResponse) {
+    return null;
+  }
+  let content;
+  if (props.entrySearchResponse.summaries.length === 0) {
+    content = (
+      <div>No known list contains {props.entrySearchResponse.query}</div>
     );
+  } else {
+    const listItems = props.entrySearchResponse.summaries.map((entry) => {
+      return (
+        <li key={entry.addedVersionId}>
+          <div className={'blocklist-name'}>{entry.blocklistName}</div>
+          {entryStatus(entry)}
+        </li>
+      );
+    });
+    content = (
+      <div>
+        <p>Blocklists that contain a matching entry, including the date it was first added, and optionally removed.</p>
+        <ul>{listItems}</ul>
+      </div>
+    )
+  }
+
+  return (
+    <section className={'component-display-entry-search-response'}>
+      <h2>
+        Lists containing {props.entrySearchResponse.query}
+      </h2>
+      {content}
+    </section>
+  );
 };
--- src/services/ApiClient.ts
+++ src/services/ApiClient.ts
@@ -2,94 +2,100 @@ import ErrorResponse from "../exceptions/ErrorResponse";
 import ApiError from "../exceptions/ApiError";
 
 export interface DnsQuery {
-    queryName: string
-    queryType: string
-    response: string
-    rcode: string
+  queryName: string
+  queryType: string
+  response: string
+  rcode: string
 }
 
 export interface EntrySearchResponse {
-    query: string
-    summaries: EntrySummary[]
+  query: string
+  summaries: EntrySummary[]
 }
 
 export interface EntrySummary {
-    blocklistName: string
-    blocklistId: string
-    addedVersionId: string
-    addedOn: Date
-    removedVersionId: string
-    removedOn: Date
+  blocklistName: string
+  blocklistId: string
+  addedVersionId: string
+  addedOn: Date
+  removedVersionId: string|null
+  removedOn: Date|null
+  listAddedOn: Date
 }
 
 export default class ApiClient {
-    private static readonly rootApiUrl = process.env.REACT_APP_ROOT_API_URL;
+  private static readonly rootApiUrl = process.env.REACT_APP_ROOT_API_URL;
 
-    private static readonly defaultHeaders = () => {
-        return {
-            'Content-Type': 'application/json'
-        }
-    };
-
-    /**
-     * Native fetch wrapped in a try/catch to return an ErrorResponse instead of throwing. The
-     * promise is always resolved and will never be rejected.
-     * @throws never
-     */
-    private static async safeFetch(input: RequestInfo, init?: RequestInit): Promise<Response | ErrorResponse> {
-        try {
-            return await fetch(input, init);
-        } catch (error) {
-            return ErrorResponse.fromError(error);
-        }
+  private static readonly defaultHeaders = () => {
+    return {
+      'Content-Type': 'application/json'
     }
+  };
 
-    public static async dnsQuery(domainName: string, queryType: string): Promise<DnsQuery|ApiError> {
-        let params = new URLSearchParams();
-        params.append('name', domainName);
-        params.append('type', queryType);
-        const response = await this.safeFetch(`${this.rootApiUrl}/dns-query?${params}`, {
-            headers: this.defaultHeaders(),
-            mode: 'cors',
-            method: 'GET'
-        });
-
-        if (response.status !== 200) {
-            let error = `DNS query failed: ${response.status} => ${await response.text()}`;
-            console.log(error);
-            return ApiError.fromMessage(error);
-        }
-        return await response.json();
+  /**
+   * Native fetch wrapped in a try/catch to return an ErrorResponse instead of throwing. The
+   * promise is always resolved and will never be rejected.
+   * @throws never
+   */
+  private static async safeFetch(input: RequestInfo, init?: RequestInit): Promise<Response | ErrorResponse> {
+    try {
+      return await fetch(input, init);
+    } catch (error) {
+      return ErrorResponse.fromError(error);
     }
+  }
 
-    public static async entrySearch(query: string): Promise<EntrySearchResponse|ApiError> {
-        let params = new URLSearchParams();
-        params.append('q', query);
-        const response = await this.safeFetch(`${this.rootApiUrl}/entries/search?${params}`, {
-            headers: this.defaultHeaders(),
-            mode: 'cors',
-            method: 'GET'
-        });
-        if (response.status !== 200) {
-            let error = `Entry search failed: ${response.status} => ${await response.text()}`;
-            console.log(error);
-            return ApiError.fromMessage(error);
-        }
-        const body = await response.json() as any;
+  public static async dnsQuery(domainName: string, queryType: string): Promise<DnsQuery | ApiError> {
+    let params = new URLSearchParams();
+    params.append('name', domainName);
+    params.append('type', queryType);
+    const response = await this.safeFetch(`${this.rootApiUrl}/dns-query?${params}`, {
+      headers: this.defaultHeaders(),
+      mode: 'cors',
+      method: 'GET'
+    });
 
-        const summaries = body.summaries.map(function(summary: any) {
-            return {
-                blocklistName: summary.blocklistName,
-                blocklistId: summary.blocklistId,
-                addedVersionId: summary.addedVersionId,
-                addedOn: new Date(summary.addedOn),
-                removedVersionId: summary.removedVersionId,
-                removedOn: new Date(summary.removedOn)
-            }
-        });
-        return {
-            query: body.query as string,
-            summaries: summaries
-        }
+    if (response.status !== 200) {
+      let error = `DNS query failed: ${response.status} => ${await response.text()}`;
+      console.log(error);
+      return ApiError.fromMessage(error);
     }
+    return await response.json();
+  }
+
+  public static async entrySearch(query: string): Promise<EntrySearchResponse | ApiError> {
+    let params = new URLSearchParams();
+    params.append('q', query);
+    const response = await this.safeFetch(`${this.rootApiUrl}/entries/search?${params}`, {
+      headers: this.defaultHeaders(),
+      mode: 'cors',
+      method: 'GET'
+    });
+    if (response.status !== 200) {
+      let error = `Entry search failed: ${response.status} => ${await response.text()}`;
+      console.log(error);
+      return ApiError.fromMessage(error);
+    }
+    const body = await response.json() as any;
+
+    const summaries = body.summaries.map(function (summary: any) {
+      let removedOn = summary.removedOn;
+      if (summary.removedOn) {
+        removedOn = new Date(summary.removedOn)
+      }
+      return {
+        blocklistName: summary.blocklistName,
+        blocklistId: summary.blocklistId,
+        addedVersionId: summary.addedVersionId,
+        addedOn: new Date(summary.addedOn),
+        removedVersionId: summary.removedVersionId,
+        removedOn: removedOn,
+        listAddedOn: new Date(summary.listAddedOn)
+      }
+    });
+    return {
+      query: body.query as string,
+      summaries: summaries
+    }
+  }
 }
