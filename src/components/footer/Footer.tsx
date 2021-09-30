1559dd10c33e139dedaf00d981e95547f49598fa
Daniel <daniel@developerdan.com>
2021-09-30 15:00:49 +0000

    Added link to github-history-generator

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -11,7 +11,10 @@ export const Footer: FunctionComponent = () =>  {
             all the versions was a key feature I wanted. I designed it to store only the differences in each list version.
             This means even if a list has a million entries, a new version of that list would only require a few hundred entries
             to generate the difference. Using the diffs, I can still generate the full list for any given version without having
-            to save the entire list. New versions are added daily and currently only uses 23 GB. Enjoy!
+            to save the entire list. New versions are added daily and currently only uses 23 GB. You might be curious where
+            I got 50,000 unique versions from. The majority of these lists are served on Github, so it was just a simple matter
+            of walking the Git history and throwing out any non-trivial changes.
+            See <a href="https://github.com/blocklist-tools/github-history-generator">github-history-generator</a> Enjoy!
           </p>
 
           Copyright &copy; 2021 Daniel White
