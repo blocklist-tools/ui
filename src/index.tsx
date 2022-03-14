9e4de7aef2a4bb60eac839a170e5a9255981dfa9
Daniel <daniel@developerdan.com>
2022-03-14 17:09:04 -0400

    Dependancy updates

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import 'react-app-polyfill/stable';
 import React from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import './index.css';
 import App from './App';
 
